0aa6c4e21d74cf0f389d5f1fa2e65d7df3047900
Héctor Rodríguez <hd.rdz98@gmail.com>
2021-11-28 01:07:56 -0600

    yes

---
+++ src/main/java/dev/sasukector/hundreddaysbase/HundredDaysDesert.java
@@ -0,0 +1,47 @@
+package dev.sasukector.hundreddaysbase;
+
+import dev.sasukector.hundreddaysbase.commands.TPSCommand;
+import dev.sasukector.hundreddaysbase.commands.ToggleDaysCommand;
+import dev.sasukector.hundreddaysbase.commands.PlayedCommand;
+import dev.sasukector.hundreddaysbase.controllers.BoardController;
+import dev.sasukector.hundreddaysbase.controllers.TeamsController;
+import dev.sasukector.hundreddaysbase.events.SpawnEvents;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public final class HundredDaysDesert extends JavaPlugin {
+
+    private static @Getter HundredDaysDesert instance;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info(ChatColor.DARK_PURPLE + "HundredDaysDesert startup!");
+        instance = this;
+
+        // Configuration
+        this.saveDefaultConfig();
+
+        // Register events
+        this.getServer().getPluginManager().registerEvents(new SpawnEvents(), this);
+        Bukkit.getOnlinePlayers().forEach(player -> BoardController.getInstance().newPlayerBoard(player));
+
+        // Register commands
+        Objects.requireNonNull(HundredDaysDesert.getInstance().getCommand("conter-tps")).setExecutor(new TPSCommand());
+        Objects.requireNonNull(HundredDaysDesert.getInstance().getCommand("played")).setExecutor(new PlayedCommand());
+        Objects.requireNonNull(HundredDaysDesert.getInstance().getCommand("toggleDays")).setExecutor(new ToggleDaysCommand());
+
+        // Start teams
+        TeamsController.getInstance();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info(ChatColor.DARK_PURPLE + "HundredDaysBedrockWalk shutdown!");
+    }
+}
---
+++ src/main/java/dev/sasukector/hundreddaysbase/events/SpawnEvents.java
@@ -0,0 +1,37 @@
+package dev.sasukector.hundreddaysbase.events;
+
+import dev.sasukector.hundreddaysbase.controllers.BoardController;
+import dev.sasukector.hundreddaysbase.controllers.TeamsController;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+
+public class SpawnEvents implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        event.joinMessage(
+                Component.text("+ ", TextColor.color(0x84E3A4))
+                        .append(Component.text(player.getName(), TextColor.color(0x84E3A4)))
+        );
+        BoardController.getInstance().newPlayerBoard(player);
+        if (player.isOp()) {
+            TeamsController.getInstance().getMasterTeam().addEntry(player.getName());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        BoardController.getInstance().removePlayerBoard(player);
+        event.quitMessage(
+                Component.text("- ", TextColor.color(0xE38486))
+                        .append(Component.text(player.getName(), TextColor.color(0xE38486)))
+        );
+    }
+
+}
